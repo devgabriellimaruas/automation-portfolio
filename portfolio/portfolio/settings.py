ad57464533e006cad8d3ae4f14215d00f1535920
devgabriellimaruas <limaruasgabriel@gmail.com>
2025-10-01 20:49:52 -0300

    update: defining news envs

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -15,7 +15,7 @@ import os
 from dotenv import load_dotenv
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-load_dotenv(BASE_DIR / '.env')
+load_dotenv(BASE_DIR.parent / '.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
